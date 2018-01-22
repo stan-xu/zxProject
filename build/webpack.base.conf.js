438a7d7ca7123106a74ae30539458f46c6fb7083
mcq <476840998@qq.com>
2018-01-22 09:10:26 +0800

    update

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -44,7 +44,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('./node_modules/_element-ui@2.0.11@element-ui/src')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
