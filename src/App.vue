6d69192ad2c4776b7abc2331796a206d7d2bacb4
mcq <476840998@qq.com>
2018-01-24 10:56:42 +0800

    add browser warn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <warn v-if="isIEVersionSupport()"/>
     <my-header></my-header>
     <router-view/>
   </div>
@@ -7,10 +8,28 @@
 
 <script>
   import myHeader from './components/myHeader.vue'
+  import Warn from './components/warn.vue'
 
   export default {
     name: 'app',
-    components: {myHeader}
+    components: {myHeader, Warn},
+    methods: {
+      isIEVersionSupport () { // 判断是否是IE10以下版本
+        const userAgent = navigator.userAgent // 取得浏览器的userAgent字符串
+        console.log(userAgent)
+        const is10AndLessIE = userAgent.indexOf('compatible') > -1 && userAgent.indexOf('MSIE') > -1 // 判断是否是包括10及以下的IE浏览器
+        if (is10AndLessIE) {
+          const reIE = new RegExp('MSIE (\\d+\\.\\d+);')
+          reIE.test(userAgent)
+          const IEVersion = parseFloat(RegExp['$1'])
+          if (IEVersion < 10) {
+            return false
+          } else {
+            return true
+          }
+        }
+      }
+    }
   }
 </script>
 
---
+++ src/components/warn.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-alert
+    :title="title"
+    :description="description"
+    type="warning"
+    close-text="知道了，仍要继续浏览">
+  </el-alert>
+</template>
+<script>
+  export default {
+    name: 'Warn',
+    data () {
+      return {
+        title: '提示: 您在使用低版本的IE浏览器，使用不兼容的浏览器可能导致浏览异常。',
+        description: '解决方法（任选其一）：升级至IE10或更高版本；安装Chrome(谷歌浏览器）或Firefox（火狐）浏览器来使用本系统。'
+      }
+    }
+  }
+</script>
