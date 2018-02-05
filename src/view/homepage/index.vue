f471e35608da3f23af127414b354534f2c4c8930
Root <940150534@qq.com>
2018-02-05 10:28:51 +0800

    add logoCarousel

--- src/view/homepage/index.vue
+++ src/view/homepage/index.vue
@@ -2,8 +2,9 @@
   <div id="homepage">
     <home-nav></home-nav>
     <home-banner></home-banner>
-    <div class="container">
-      <company-list></company-list>
+    <div class="container" v-if="loaded">
+      <company-list v-if="isLogin"></company-list>
+      <logo-carousel v-else></logo-carousel>
     </div>
     <my-footer></my-footer>
   </div>
@@ -14,15 +15,34 @@
   import HomeNav from './homeNav'
   import CompanyList from '../../components/company-list/index'
   import HomeBanner from './homeBanner'
+  import LogoCarousel from './logoCarousel'
 
   export default {
     components: {
+      LogoCarousel,
       HomeBanner,
       CompanyList,
       HomeNav,
       MyFooter
     },
-    name: 'homepage'
+    name: 'homepage',
+    data () {
+      return {
+        isLogin: false,
+        loaded: false
+      }
+    },
+    mounted () {
+      this.$api.get('/account/islogin', null,
+        resj => {
+          if (resj.message === '未登录') {
+            this.isLogin = false
+          } else {
+            this.isLogin = true
+          }
+          this.loaded=true
+        })
+    }
   }
 </script>
 
---
+++ src/view/homepage/logoCarousel.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="logo-carousel" v-loading="loading">
+    <el-carousel height="296px">
+      <el-carousel-item v-for="(i,index) in pages" :key="index">
+        <el-row :gutter="20">
+          <el-col :span="6" v-for="(item,index) in listInfo[i-1]" :key="index">
+            <div  class="item-logo center-block">
+              <img :src="baseUrl+'/uploadify/renderThumb/'+item.ent_logo+'/150x150'" alt="" class="img-responsive">
+            </div>
+          </el-col>
+        </el-row>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'logo-carousel',
+    data () {
+      return {
+        listInfo: [],
+        loading: true,
+        pages: 0
+      }
+    },
+    mounted () {
+      this.$api.get('/ent/query', {pagesize: 24},
+        resj => {
+          this.pages = Math.ceil(resj.rows.length / 8)
+          for (let i = 0; i < this.pages; i++) {
+            this.listInfo[i] = []
+            for (let j = 0; j < 8; j++) {
+              let item = resj.rows.shift()
+              if (item) {
+                this.listInfo[i][j] = item
+              } else {
+                break
+              }
+            }
+          }
+          // this.listInfo = resj
+          this.loading = false
+        })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  #logo-carousel{
+   padding: 50px;
+    .item-logo {
+      border: 1px solid #ddd;
+      line-height: 136px;
+      text-align: center;
+      margin-bottom: 20px;
+      img {
+        display: inline-block;
+        vertical-align: middle;
+      }
+    }
+  }
+</style>
