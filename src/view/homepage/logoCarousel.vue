5e58a7a3c1149095f8cec5fa5809dadc466424db
Root <940150534@qq.com>
2018-02-06 13:36:03 +0800

    修改轮播切换时间

--- src/view/homepage/logoCarousel.vue
+++ src/view/homepage/logoCarousel.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="logo-carousel" v-loading="loading">
-    <el-carousel height="296px">
+    <el-carousel height="296px" interval="20000">
       <el-carousel-item v-for="(i,index) in pages" :key="index">
         <el-row :gutter="20">
           <el-col :span="6" v-for="(item,index) in listInfo[i-1]" :key="index">
-            <div  class="item-logo center-block">
+            <div class="item-logo center-block">
+              <img :src="baseUrl+'/uploadify/renderThumb/'+item.ent_logo+'/136x136'" alt="" class="img-responsive">
+            </div>
+          </el-col>
+        </el-row>
+      </el-carousel-item>
+      <el-carousel-item v-for="(i,index) in pages" :key="index">
+        <el-row :gutter="20">
+          <el-col :span="6" v-for="(item,index) in listInfo[i-1]" :key="index">
+            <div class="item-logo center-block">
               <img :src="baseUrl+'/uploadify/renderThumb/'+item.ent_logo+'/136x136'" alt="" class="img-responsive">
             </div>
           </el-col>
@@ -47,8 +56,8 @@
 </script>
 
 <style lang="scss" scoped>
-  #logo-carousel{
-   padding: 50px;
+  #logo-carousel {
+    padding: 50px;
     .item-logo {
       border: 1px solid #ddd;
       line-height: 136px;
