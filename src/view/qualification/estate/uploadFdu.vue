9d51784e2170aaf7f44465849c6d0b001ddcb17f
e <707571699@qq.com>
2018-03-05 15:46:48 +0800

    消防设计单位资质类型修改

--- src/view/qualification/estate/uploadFdu.vue
+++ src/view/qualification/estate/uploadFdu.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="upload1" v-cloak v-if="loading">
     <uploadState  v-if="data" :data = 'data'></uploadState>
-    <p v-if="!data||data.sign_status=='审核未通过'">(说明:消防设计单位资质证书为一级，二级，三级，四级。）</p>
+    <p v-if="!data||data.sign_status=='审核未通过'">(说明:消防设计单位资质证书分为设计资质专项甲级或乙级，设计与施工一体化资质一级或二级。）</p>
     <uploadForm v-if="!data||data.sign_status=='审核未通过'" :data = 'data' :name='name' @load="load" :levelLsit='levelLsit'
                 :levelAdd='levelAdd' :example='example'>
     </uploadForm>
@@ -22,8 +22,8 @@
     data () {
       return {
         loading: false,
-        levelLsit: ['综合甲级', '建筑甲级', '建筑乙级'],
-        levelAdd: 7, // 等级修正
+        levelLsit: ['专项甲级', '专项乙级', '一体化资质一级', '一体化资质二级'],
+        levelAdd: [7,8,9,23], // 等级修正
         data: '',  // 已上传的信息
         name: '消防设计单位等级类型',
         example: require('./1_cert.png') // 示例图片,
